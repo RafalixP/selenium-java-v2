631d6a0f63c793ec769f67e96d115da46f7a15bd
RafalixP <pieczka.r@gmail.com>
2026-01-20 11:10:38 +0100

    Dodałem drugi test case

--- src/test/java/com/rafal/selenium/tests/CheckboxesTest.java
+++ src/test/java/com/rafal/selenium/tests/CheckboxesTest.java
@@ -7,6 +7,7 @@ import com.rafal.selenium.pages.CheckboxesPage;
 
 public class CheckboxesTest extends BaseTest {
     @Test
+    public void isBoxChecked() {
     //case 1
 
     //wchodzimy na podstronę Checkboxes
@@ -23,4 +24,30 @@ public class CheckboxesTest extends BaseTest {
 
     //sprawdamy czy box #1 jest zaznaczony (czyli że zmienił się jego stan po naszym kliknięciu)
     Assert.assertTrue(page.isCheckboxSelected(0));
+    }
+
+    @Test
+    //case 2 - sprawdzenie czy jak zaznaczę boxa #1 i potem ODzaznaczę #2 to czy #1 nadal jest zaznaczony
+
+    // sprawdzamy czy box #1 NIE jest zaznaczony
+    Assert.assertFalse(page.isCheckboxSelected(0));
+
+    //klikam #1 boxa z listy
+    page.clickCheckbox(0);
+
+    // sprawdzamy czy box #1 jest zaznaczony
+    Assert.assertTrue(page.isCheckboxSelected(0));
+
+    //sprawdzamy czy box #2 jest zaznaczony (domyślnie)
+    Assert.assertTrue(page.isCheckboxSelected(1));
+
+    //klikam drugiego boxa z listy
+    page.clickCheckbox(1);
+
+    //sprawdzam czy box #2 NIE jest już zaznaczony
+    Assert.assertFalse(page.isCheckboxSelected(1));
+
+    //sprawdzam czy box #1 nadal JEST zaznaczony
+    Assert.assertTrue(page.isCheckboxSelected(0));
+    
 }
