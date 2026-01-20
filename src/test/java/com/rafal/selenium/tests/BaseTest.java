a31877a59bdab58271d4c79d4403cdec9ed17073
RafalixP <pieczka.r@gmail.com>
2026-01-20 13:38:57 +0100

    dodałem waita

--- src/test/java/com/rafal/selenium/tests/BaseTest.java
+++ src/test/java/com/rafal/selenium/tests/BaseTest.java
@@ -2,16 +2,20 @@ package com.rafal.selenium.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait; // do obsługi waita
+import java.time.Duration;  // do obsługi waita
 import org.testng.annotations.BeforeMethod; // do @BeforeMethod
 import org.testng.annotations.AfterMethod;  // do @AfterMethod
 import org.openqa.selenium.By;              // do znajdowania elementów
 
 public class BaseTest {
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.get("https://the-internet.herokuapp.com");
     }
 
