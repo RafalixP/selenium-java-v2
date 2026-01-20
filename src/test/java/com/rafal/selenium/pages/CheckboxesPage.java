8aa2ae8bd08b5655457eaf3dd84be4c4361847ae
RafalixP <pieczka.r@gmail.com>
2026-01-20 10:03:47 +0100

    Gotowy, prosty POM dla checkboxów

--- src/test/java/com/rafal/selenium/pages/CheckboxesPage.java
+++ src/test/java/com/rafal/selenium/pages/CheckboxesPage.java
@@ -1,5 +1,31 @@
 package com.rafal.selenium.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+
 public class CheckboxesPage extends BasePage {
-    
+    //lokatory
+    //stała lokalizująca checkboxy
+    private static final By CHECKBOXES_LOCATOR = By.cssSelector("input[type='checkbox']"); 
+
+    //konstruktor
+    public CheckboxesPage(WebDriver driver) {
+        super(driver);  
+    }
+    //metody
+
+    //stwórz listę checkboxów korzystając z podanego lokatora, następnie kliknj box o numerze podanym jako parametr metody
+    public void clickCheckbox(int index) {
+        List<WebElement> checkboxes = driver.findElements(CHECKBOXES_LOCATOR);
+        checkboxes.get(index).click();
+    }
+
+    // stwórz listę checkboxów korzystając z podanego lokatora (duplikacja kodu!), następnie zwróć stan boxa (True/False)
+    public boolean isCheckboxSelected(int index) {
+        List<WebElement> checkboxes = driver.findElements(CHECKBOXES_LOCATOR);
+        return checkboxes.get(index).isSelected();
+    }
+
 }
