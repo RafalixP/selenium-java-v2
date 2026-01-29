21782a3448d2fecd457137c690f0a65d8e3320b8
RafalixP <pieczka.r@gmail.com>
2026-01-29 11:45:53 +0100

    drobna reorganizacja, poprawiam komentarze

--- src/test/java/com/rafal/selenium/tests/DropdownTest.java
+++ src/test/java/com/rafal/selenium/tests/DropdownTest.java
@@ -19,27 +19,28 @@ public class DropdownTest extends BaseTest {
         driver.findElement(By.linkText("Dropdown")).click(); //znajdujemy link do podstrony i klikamy go
     }
 
-    // case - Po załadowaniu strony żadna opcja nie jest wybrana
+    // case 1 - Po załadowaniu strony żadna opcja nie jest wybrana
     @Test
-    public void defaultState() {
-        Assert.assertEquals(page.getSelectedOptionText(), "Please select an option");
+    public void defaultState_shouldShowPlaceholder() {
+        Assert.assertEquals(page.getSelectedOptionText(), "Please select an option", "Domyślnie dropdown powinien pokazywać placeholder 'Please select an option'"); // komunikat na wypadek gdyby nie było równości
     }
         
-
-    // case - Odświeżenie strony nie zmienia stanu (nic nie jest wybrane)
-
-    // case - Pole dropdowna nie jest edytowalne ręcznie
-
-    // case - Kliknięcie opcji 1 → opcja ma selected
+    // case 2 - Kliknięcie opcji 1 → opcja ma selected
     @Test
     public void selectDropdown() {
         
         page.selectByVisibleText("Option 1");
 
         //Assert.assertTrue(driver.findElement(By.id("dropdown")).isDisplayed()); // to jest tu niepotrzebne, bo nas interesuje wybrana opcja, a nie czy box się wyświetlił
-        Assert.assertEquals(page.getSelectedOptionText(), "Option 1");
+        Assert.assertEquals(page.getSelectedOptionText(), "Option 1"); 
 
     }
+
+    // case - Odświeżenie strony nie zmienia stanu (nic nie jest wybrane)
+
+    // case - Pole dropdowna nie jest edytowalne ręcznie
+
+    
 
     // case - Kliknięcie opcji 2 → opcja ma selected
 
