a585c3b9e1723845cf29a38239644fa1cc309094
RafalixP <pieczka.r@gmail.com>
2026-01-29 09:47:03 +0100

    usuwam niepotrzebny import

--- src/test/java/com/rafal/selenium/listeners/ScreenshotListener.java
+++ src/test/java/com/rafal/selenium/listeners/ScreenshotListener.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.io.FileHandler;
-import org.testng.ITestContext; 
+//import org.testng.ITestContext; 
 import org.testng.ITestListener; 
 import org.testng.ITestResult; 
 
