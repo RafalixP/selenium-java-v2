29e6862197ac41d6bc7113814fe5145193fc6eb3
RafalixP <pieczka.r@gmail.com>
2026-02-13 10:28:08 +0100

    usunąłem nieprawidłowy import

--- src/test/java/com/rafal/selenium/tests/HoversTest.java
+++ src/test/java/com/rafal/selenium/tests/HoversTest.java
@@ -1,7 +1,6 @@
 package com.rafal.selenium.tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Int;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.Assert;
