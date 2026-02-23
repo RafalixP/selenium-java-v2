c35cf8a575de8a9374310b32a2a8afd89d31d8d9
RafalixP <pieczka.r@gmail.com>
2026-02-23 12:10:25 +0100

    wyłączyłem WAIT_TIME

--- src/test/java/com/rafal/selenium/pages/HomePage.java
+++ src/test/java/com/rafal/selenium/pages/HomePage.java
@@ -9,30 +9,47 @@ public class HomePage extends BasePage {
     private By loginLink =  By.linkText("Form Authentication");
     private By checkboxesLink = By.linkText("Checkboxes");
     private By hoversLink = By.linkText("Hovers");
+    private By dynamicLoadingLink = By.linkText("Dynamic Loading");
+
+    private final Duration WAIT_TIME = Duration.ofSeconds(5);   // stała
 
     //konstruktor
     public HomePage(WebDriver driver) {
         super(driver);
     }
 
+    // metody
+
+    // kliknij dopiero kiedy element da się kliknąć
+    private void clickWhenReady(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, WAIT_TIME);
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    } 
+
     public DropdownPage goToDropdownPage() {
-        driver.findElement(dropdownLink).click();
+        driver.clickWhenReady(dropdownLink);
         return new DropdownPage(driver);
     }
 
     public LoginPage goToLoginPage() {
-        driver.findElement(loginLink).click();
+        driver.clickWhenReady(loginLink);
         return new LoginPage(driver);
     }
 
     public CheckboxesPage goToCheckboxesPage() {
-        driver.findElement(checkboxesLink).click();
+        driver.clickWhenReady(checkboxesLink);
         return new CheckboxesPage(driver);
     }
 
     public HoversPage goToHoversPage() {
-        driver.findElement(hoversLink).click();
+        driver.clickWhenReady(hoversLink);
         return new HoversPage(driver);
+    }
+
+    public DynamicLoadingPage goToDynamicLoadingPage() {
+        driver.clickWhenReady(dynamicLoadingLink);
+        return new DynamicLoadingPage(driver);
     }
     
 }
