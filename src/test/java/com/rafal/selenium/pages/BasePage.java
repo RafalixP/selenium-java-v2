a1386e3dbdd7273f0120ac5aab7e94e62a6f70b4
RafalixP <pieczka.r@gmail.com>
2026-02-24 14:47:44 +0100

    zastąpiłem WAIT_TIME metodą clickWhenReady(By locator) przeniesioną z HomePage

--- src/test/java/com/rafal/selenium/pages/BasePage.java
+++ src/test/java/com/rafal/selenium/pages/BasePage.java
@@ -1,16 +1,26 @@
 package com.rafal.selenium.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class BasePage {
     protected WebDriver driver;
     protected WebDriverWait wait;
-    private final Duration WAIT_TIME = Duration.ofSeconds(5);   // stała
+    //protected final Duration WAIT_TIME = Duration.ofSeconds(5);   // stała
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
+
+    // kliknij dopiero kiedy element da się kliknąć
+    public void clickWhenReady(By locator) {
+        //WebDriverWait wait = new WebDriverWait(driver, WAIT_TIME);
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    } 
 }
