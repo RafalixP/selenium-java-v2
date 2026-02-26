05e1b415cf48ede30c427e70f5e613def354aed5
RafalixP <pieczka.r@gmail.com>
2026-02-26 14:40:49 +0100

    przesuwam waita do pliku config

--- src/test/java/com/rafal/selenium/pages/BasePage.java
+++ src/test/java/com/rafal/selenium/pages/BasePage.java
@@ -1,20 +1,27 @@
 package com.rafal.selenium.pages;
+import com.rafal.selenium.utils.ConfigReader;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 import java.time.Duration;
 
 public class BasePage {
     protected WebDriver driver;
     protected WebDriverWait wait;
-    protected final Duration DEFAULT_TIME = Duration.ofSeconds(10);   // stała
+    // protected final Duration DEFAULT_TIME = Duration.ofSeconds(10);   // stała
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, DEFAULT_TIME);
+
+        //pobieramy czas oczekiwania z config i konwertujemy na Duration
+        int waitSeconds = ConfigReader.getInt("waitTime");
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(waitSeconds));
+        //this.wait = new WebDriverWait(driver, DEFAULT_TIME);
     }
 
     // kliknij dopiero kiedy element da się kliknąć
