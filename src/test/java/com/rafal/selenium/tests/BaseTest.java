eb127124e6dcbfe175d7b5ac3f988547ea30e69a
RafalixP <pieczka.r@gmail.com>
2026-02-26 15:44:20 +0100

    dodaję  obsługę WebDriverManagera (chociaż wcale nie jest potrzebna w tej wersji selenium

--- src/test/java/com/rafal/selenium/tests/BaseTest.java
+++ src/test/java/com/rafal/selenium/tests/BaseTest.java
@@ -4,11 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.support.ui.WebDriverWait; // do obsługi waita
-//import java.time.Duration;  // do obsługi waita
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.BeforeMethod; // do @BeforeMethod
 
-//import com.rafal.selenium.pages.LoginPage;
+
 import com.rafal.selenium.utils.ConfigReader;
 
 import org.testng.annotations.AfterMethod;  // do @AfterMethod
@@ -30,13 +29,16 @@ public class BaseTest {
 
         switch (browser.toLowerCase()) {
             case "chrome":
+                WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 break;
             case "firefox":
+                WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
                 break;
 
             case "edge":
+                WebDriverManager.edgedriver().setup();
                 driver = new EdgeDriver();
                 break;
             default:
